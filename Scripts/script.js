1b2bb90152b3a2e528f9b5c48fe807e1b4725993
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-08-29 13:46:27 -0300

    Fix #8, fix #9: clock and logos load correctly + lightweight logo.
    There was a missing google API that made the `ready` function fail.
    Removing the usage of google API was enough.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,118 +1,10 @@
 $(document).ready(function () {
-
-   $('.beat').viewportChecker({
+    $('.beat').viewportChecker({
         classToAdd: 'pulsar',
         offset: 100
     });
 
     // map setup
-    var latlng = new google.maps.LatLng(54.445340, 18.553448);
-    function initialize() {
-        var mapProp = {
-            center: latlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false,
-            styles: [
-                      {
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#f5f5f5"}]
-                      },
-                      {
-                          "elementType": "labels.icon",
-                          "stylers": [{"visibility": "off"}]
-                      },
-                      {
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#616161"}]
-                      },
-                      {
-                          "elementType": "labels.text.stroke",
-                          "stylers": [{"color": "#f5f5f5"}]
-                      },
-                      {
-                          "featureType": "administrative.land_parcel",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#bdbdbd"}]
-                      },
-                      {
-                          "featureType": "poi",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#eeeeee"}]
-                      },
-                      {
-                          "featureType": "poi",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#757575"}]
-                      },
-                      {
-                          "featureType": "poi.park",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#e5e5e5"}]
-                      },
-                      {
-                          "featureType": "poi.park",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#9e9e9e"}]
-                      },
-                      {
-                          "featureType": "road",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#ffffff"}]
-                      },
-                      {
-                          "featureType": "road.arterial",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#757575"}]
-                      },
-                      {
-                          "featureType": "road.highway",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#dadada"}]
-                      },
-                      {
-                          "featureType": "road.highway",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#616161"}]
-                      },
-                      {
-                          "featureType": "road.local",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#9e9e9e"}]
-                      },
-                      {
-                          "featureType": "transit.line",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#e5e5e5"}]
-                      },
-                      {
-                          "featureType": "transit.station",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#eeeeee"}]
-                      },
-                      {
-                          "featureType": "water",
-                          "elementType": "geometry",
-                          "stylers": [{"color": "#c9c9c9"}]
-                      },
-                      {
-                          "featureType": "water",
-                          "elementType": "labels.text.fill",
-                          "stylers": [{"color": "#9e9e9e"}]
-                      }
-            ]
-        };
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            icon: 'Images/marker.png'
-        });
-
-        var map = new google.maps.Map(document.getElementById("mapa"), mapProp);
-        marker.setMap(map);
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-
     toggleTimerVisibility();
     toggleWorkshopsLayout();
     toggleBrandImageSource();
@@ -194,7 +86,7 @@ $(window).scroll(function (e) {
     else{
         $('.upper').slideUp();
     }
-    
+
     if ($('#navbar').hasClass("in")) {
         $('#navbar').removeClass("in");
         $('.navbar-toggle').removeClass('focused');
@@ -250,10 +142,10 @@ function toggleWorkshopsLayout() {
 
 function toggleBrandImageSource() {
     if (innerWidth <= 320) {
-        $('#brand-image').attr('src', 'Images/new-logo.png');
+        $('#brand-image').attr('src', 'Images/new-logo.jpg');
     }
     else {
-        $('#brand-image').attr('src', 'Images/new-logo.png');
+        $('#brand-image').attr('src', 'Images/new-logo.jpg');
     }
 }
 
@@ -265,10 +157,10 @@ $(window).resize(function () {
 });
 
 
-$('#days').countdown('2017/06/28 9:15:00', function (event) {
-    $(this).html(event.strftime('%D') + '<br><small style="font-size: 0.6em">days<small>');
-    $('#hours').html(event.strftime('%H ')  + '<br><small style="font-size: 0.6em">hours<small>');
-    $('#minutes').html(event.strftime('%M ') + '<br><small style="font-size: 0.6em;">minutes<small>');
+$('#days').countdown('2019/09/30 8:20:00', function (event) {
+    $(this).html(event.strftime('%D') + '<br><small style="font-size: 0.6em">dias<small>');
+    $('#hours').html(event.strftime('%H ')  + '<br><small style="font-size: 0.6em">horas<small>');
+    $('#minutes').html(event.strftime('%M ') + '<br><small style="font-size: 0.6em;">minutos<small>');
 });
 
 function showcontent(title, desc, speach, src, company, workshop) {
