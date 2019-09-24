98ceaf417f9580ee4d15379da3aca7d1b1402849
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-09-24 03:49:15 -0300

    Table-style grid.

--- generate.py
+++ generate.py
@@ -10,6 +10,7 @@ def generate(data, template: Path, output: Path):
     env = Environment(
         loader=FileSystemLoader(searchpath=[str(Path(__file__).parent.resolve())])
     )
+    env.filters['is_list'] = lambda x: isinstance(x, list)
 
     template = env.get_template(template.name)
 
